920d05a593ba75f74dfe839ec7cbca087dee91bf
alejandra121526 <margaritamamian2001@gmail.com>
2024-07-23 11:26:54 -0500

    ajust

--- backend/routes/usuarioRoutes.js
+++ backend/routes/usuarioRoutes.js
@@ -3,6 +3,7 @@ const router = express.Router();
 
 import { registrar } from "../controllers/usuarioController.js"
 
+
 //autenticacion, registro y confirmacion de usuarios
 router.post('/', registrar); //creacion de usuarios
 
